c3a3af611e701f13fc8cc056911bd25bd9f248cd
Juri <lubonjajurgen1@gmail.com>
2023-09-17 09:21:41 +0200

    Header + Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = 'Movies App';
+  title = 'Movies App'
 
-  today: Date = new Date()
-
-  imageUrl: string = 'https://dotnethow.net/images/movies/movie-1.jpeg'
-  imageWidth: number = 100
-
-  multipleClasses = 'sample-color sample'
-
-  price: number = 10
-  sellingPrice: number = 15.30
 }
