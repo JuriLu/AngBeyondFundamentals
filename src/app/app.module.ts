b732ae7377b8b3ec1b2369aaf129d31c041cf4e6
JurgenLubonja <jurgen.lubonja@eng.it>
2023-09-19 10:36:09 +0200

    Adding Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { DetailsComponent } from './components/details/details.component';
-import { DetailsMoviesComponent } from './components/details-movies/details-movies.component';
-import { DetailsReviewsComponent } from './components/details-reviews/details-reviews.component';
+import {DetailsComponent} from './components/details/details.component';
+import {DetailsMoviesComponent} from './components/details-movies/details-movies.component';
+import {DetailsReviewsComponent} from './components/details-reviews/details-reviews.component';
 import {NavbarComponent} from "./shared/navbar/navbar.component";
 import {HomeComponent} from "./shared/home/home.component";
 import {FooterComponent} from "./shared/footer/footer.component";
 import {NotAuthorizedComponent} from "./shared/not-authorized/not-authorized.component";
 import {LoaderComponent} from "./shared/loader/loader.component";
 import {ModalComponent} from "./shared/modal/modal.component";
-import { ProfitPipe } from './pipes/profit.pipe';
-import { DetailsActorsComponent } from './components/details-actors/details-actors.component';
-import { SearchComponent } from './shared/search/search.component';
+import {ProfitPipe} from './pipes/profit.pipe';
+import {DetailsActorsComponent} from './components/details-actors/details-actors.component';
+import {SearchComponent} from './shared/search/search.component';
+import {RouterModule} from "@angular/router";
+import {AppRoutesModule} from "./modules/app.routes";
 
 
 @NgModule({
@@ -34,9 +36,11 @@ import { SearchComponent } from './shared/search/search.component';
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
