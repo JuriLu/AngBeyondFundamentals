9c85d764e87595f08deb83b5d07ea473b5e8dc7c
JurgenLubonja <jurgen.lubonja@eng.it>
2023-09-19 12:17:49 +0200

    looping through movies with ngFor

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,5 +1,14 @@
 import {Component} from '@angular/core';
 
+interface MovieInterface {
+  imageUrl:string,
+  title:string,
+  actors:string,
+  year:string,
+  rate:number,
+  trophy:number
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,8 +16,58 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
 
-  successColor: string = 'text text-success'
-  errorColor: string = 'text text-danger'
-  isSuccess: boolean = false
+  constructor() {
+  }
 
+  fanFavouriteMovies: MovieInterface[] =
+    [
+      {
+        imageUrl: '../../../assets/images/Alien.jpg',
+        title: 'Alien',
+        actors: 'Movie Actor',
+        year: '2023',
+        rate: 9.5,
+        trophy: 1
+      },
+      {
+        imageUrl: '../../../assets/images/BlackPanther.jpg',
+        title: 'BlackPanther',
+        actors: 'Movie Actor',
+        year: '2023',
+        rate: 9.5,
+        trophy: 1
+      },
+      {
+        imageUrl: '../../../assets/images/Moonlight.jpg',
+        title: 'Moonlight',
+        actors: 'Movie Actor',
+        year: '2023',
+        rate: 9.5,
+        trophy: 1
+      },
+      {
+        imageUrl: '../../../assets/images/Oppenheimer.jpeg',
+        title: 'Oppenheimer',
+        actors: 'Movie Actor',
+        year: '2023',
+        rate: 9.5,
+        trophy: 1
+      },
+      {
+        imageUrl: '../../../assets/images/TheWalk.jpg',
+        title: 'TheWalk',
+        actors: 'Movie Actor',
+        year: '2023',
+        rate: 9.5,
+        trophy: 1
+      },
+      {
+        imageUrl: '../../../assets/images/Titanic.jpg',
+        title: 'Titanic',
+        actors: 'Movie Actor',
+        year: '2023',
+        rate: 9.5,
+        trophy: 1
+      },
+    ]
 }
